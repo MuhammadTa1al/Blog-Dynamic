81ccf4c36bf97f78ad28bed75cbc354366361c08
Muhammad Talal <balochshab248@gmail.com>
2025-01-02 23:25:55 +0500

    first commit

---
+++ sanity/lib/demo.ts
@@ -0,0 +1,24 @@
+/**
+ * Demo data used as placeholders and initial values for the blog
+ */
+
+export const title = "Talal&apos;s Blog.";
+
+export const description = [
+  {
+    _key: "9f1a629887fd",
+    _type: "block",
+    children: [
+      {
+        _key: "4a58edd077880",
+        _type: "span",
+        marks: [],
+        text: "Welcome to Meer Talal&apos;s Blog.",
+      },
+    ],
+    
+    style: "normal",
+  },
+];
+
+export const ogImageTitle = "A Next.js Blog with a Native Authoring Experience";
